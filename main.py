76f19f960b0f279f818723b1bdb930ea8a45bd4e
beka toro <bekakikalishvili18@gmail.com>
2025-12-01 21:11:53 +0100

    Gym Programm System

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app= FastAPI(titele="Now Api for Gym")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+list={"name":"biceps","repetition":3}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/catalog")
+def catalog():
+    return list
+
